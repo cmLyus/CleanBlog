cee4a24a5338732ac12cbd022ff9ba50d76d19a4
cmLyus <cemal@lupi.dev>
2023-12-02 23:37:51 +0300

    static eklendi- template engine yapılandırıldı

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+// Middlewares
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// Routes
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
+    
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    
+    res.render('about');
+});
+
+app.get('/post', (req, res) => {
+    
+    res.render('post');
+});
+
+app.get('/add_post', (req, res) => {
+    
+    res.render('add_post');
 });
 
 
