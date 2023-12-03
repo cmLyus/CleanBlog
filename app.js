368f9d377ab5e054503e621cc97d9496b2dc5b2e
cmLyus <cemal@lupi.dev>
2023-12-03 18:03:38 +0300

    mongoose ile veritabanı bağlandı. post ve get işlemleri yapıldı

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/cleanblog-test-db')
 
 // Middlewares
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-    
-    res.render('index');
+app.get('/', async (req, res) => {
+    const posts = await Post.find({});
+    res.render('index', {
+        posts
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -28,6 +37,11 @@ app.get('/add_post', (req, res) => {
     res.render('add_post');
 });
 
+app.post('/posts', async (req, res) => {
+    await Post.create(req.body);
+    res.redirect('/');
+    
+});
 
 
 const port = 3000;
