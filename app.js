2e5f5d1df2d51fd362726b29cecc5e5d20ee7f33
cmLyus <cemal@lupi.dev>
2023-12-03 19:02:49 +0300

    tekil post sayfaları oluşturuldu

--- app.js
+++ app.js
@@ -27,14 +27,16 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/post', (req, res) => {
-    
-    res.render('post');
-});
-
 app.get('/add_post', (req, res) => {
     
     res.render('add_post');
+});
+
+app.get('/posts/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('post', {
+        post
+    });
 });
 
 app.post('/posts', async (req, res) => {
